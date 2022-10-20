15de541b2293b46152b5b11c2ba87d1c5824cf8d
Веселов Дмитрий <veselov.da@reliab.tech>
2022-10-20 14:40:38 +0300

    refactor: fix delayed init

--- src/main/java/ru/text/tree/persistance/model/TreeNode.java
+++ src/main/java/ru/text/tree/persistance/model/TreeNode.java
@@ -5,21 +5,21 @@ import java.util.Collection;
 import java.util.List;
 
 public class TreeNode {
-    private List<TreeNode> children;
-     private final String name;
-    public Collection<TreeNode> getChildren(){
-        if(children==null){
-            children =new ArrayList<>();
-        }
+    private final List<TreeNode> children;
+    private final String name;
+
+    public Collection<TreeNode> getChildren() {
         return children;
+    }
+
+    public TreeNode(String name) {
+        this.name = name;
+        children = new ArrayList<>();
     }
 
     @Override
     public String toString() {
         return name;
-    }
-    public TreeNode(String name){
-        this.name=name;
     }
 
 }
